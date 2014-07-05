4bda92e4f26e3d7761b8786a9db5b65c139fe21f
TheQuestor <thequestor@gmail.com>
2014-07-05 06:18:15 -0500

    Working on a splash screen
    It's butt ugly right now, just making sure timing and functionality is
    working

---
+++ AMP Log Analiser x1/Forms/SplashScreen.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SplashScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GLabel2 = New gLabel.gLabel()
+        Me.SuspendLayout()
+        '
+        'GLabel2
+        '
+        Me.GLabel2.Feather = 255
+        Me.GLabel2.FillTypeLinear = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal
+        Me.GLabel2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GLabel2.Font = New System.Drawing.Font("Verdana", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GLabel2.ForeColor = System.Drawing.Color.White
+        Me.GLabel2.GlowColor = System.Drawing.Color.Black
+        Me.GLabel2.Location = New System.Drawing.Point(50, 42)
+        Me.GLabel2.Name = "GLabel2"
+        Me.GLabel2.Pulse = True
+        Me.GLabel2.ShadowColor = System.Drawing.Color.Black
+        Me.GLabel2.ShadowOffset = New System.Drawing.Point(6, 5)
+        Me.GLabel2.ShadowState = True
+        Me.GLabel2.Size = New System.Drawing.Size(463, 42)
+        Me.GLabel2.TabIndex = 3
+        Me.GLabel2.Text = "ArduPilot Log File Analyzer"
+        Me.GLabel2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.GLabel2.UseCompatibleTextRendering = True
+        '
+        'SplashScreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DimGray
+        Me.ClientSize = New System.Drawing.Size(562, 402)
+        Me.Controls.Add(Me.GLabel2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "SplashScreen"
+        Me.Text = "SplashScreen"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GLabel2 As gLabel.gLabel
+End Class
---
+++ AMP Log Analiser x1/Forms/SplashScreen.vb
@@ -0,0 +1,73 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Drawing.Text
+Imports System.Drawing.Drawing2D
+Imports System.Collections
+Imports System.ComponentModel
+Imports System.Windows.Forms
+Imports System.Data
+Imports gLabel
+
+Public Class SplashScreen
+#Region "     Form Position Code Start & Constants "
+    'Form Position Code Start
+    Public Const WM_NCLBUTTONDOWN As Integer = &HA1
+    Public Const HTBORDER As Integer = 18
+    Public Const HTBOTTOM As Integer = 15
+    Public Const HTBOTTOMLEFT As Integer = 16
+    Public Const HTBOTTOMRIGHT As Integer = 17
+    Public Const HTCAPTION As Integer = 2
+    Public Const HTLEFT As Integer = 10
+    Public Const HTRIGHT As Integer = 11
+    Public Const HTTOP As Integer = 12
+    Public Const HTTOPLEFT As Integer = 13
+    Public Const HTTOPRIGHT As Integer = 14
+    Public Const EM_GETLINECOUNT = &HBA
+    Public Const EM_LINESCROLL = &HB6
+
+    Private Sub SavePosition(ByVal frm As Form, ByVal app_name As String)
+        SaveSetting(app_name, "Geometry", "WindowState", frm.WindowState)
+        If frm.WindowState = FormWindowState.Normal Then
+            SaveSetting(app_name, "Geometry", "Left", frm.Left)
+            SaveSetting(app_name, "Geometry", "Top", frm.Top)
+            SaveSetting(app_name, "Geometry", "Width", frm.Width)
+            SaveSetting(app_name, "Geometry", "Height", frm.Height)
+        Else
+            SaveSetting(app_name, "Geometry", "Left", frm.RestoreBounds.Left)
+            SaveSetting(app_name, "Geometry", "Top", frm.RestoreBounds.Top)
+            SaveSetting(app_name, "Geometry", "Width", frm.RestoreBounds.Width)
+            SaveSetting(app_name, "Geometry", "Height", frm.RestoreBounds.Height)
+        End If
+    End Sub
+    Private Sub RestorePosition(ByVal frm As Form, ByVal app_name As String)
+        frm.SetBounds( _
+            GetSetting(app_name, "Geometry", "Left", Me.RestoreBounds.Left), _
+            GetSetting(app_name, "Geometry", "Top", Me.RestoreBounds.Top), _
+            GetSetting(app_name, "Geometry", "Width", Me.RestoreBounds.Width), _
+            GetSetting(app_name, "Geometry", "Height", Me.RestoreBounds.Height) _
+        )
+        Me.WindowState = GetSetting(app_name, "Geometry", "WindowState", Me.WindowState)
+    End Sub
+    'Form Position Code End
+
+
+#End Region
+
+    Private Sub SplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        RestorePosition(Me, "Splash")
+        Dim t As Timer = New Timer()
+        t.Interval = 2000
+        AddHandler t.Tick, AddressOf HandleTimerTick
+        t.Start()
+    End Sub
+    Private Sub SplashScreen_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        SavePosition(Me, "Splash")
+
+    End Sub
+    Private Sub HandleTimerTick(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        frmMainForm.Show()
+        Me.Close()
+    End Sub
+
+End Class
