817342fb93d7a65d56157a554a3ffca0d2b8c455
bionicbone <github@bionicbone.plus.com>
2014-07-06 10:21:18 +0100

    Issue Resolved
    Splash Screen should now work

--- AMP Log Analiser x1/My Project/Application.Designer.vb
+++ AMP Log Analiser x1/My Project/Application.Designer.vb
@@ -27,17 +27,12 @@ Namespace My
             Me.IsSingleInstance = false
             Me.EnableVisualStyles = true
             Me.SaveMySettingsOnExit = true
-            Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
+            Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterAllFormsClose
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.APMLogFileAnaliser.frmMainForm
-        End Sub
-        
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
-        Protected Overrides Sub OnCreateSplashScreen()
-            Me.SplashScreen = Global.APMLogFileAnaliser.SplashScreen
+            Me.MainForm = Global.APMLogFileAnaliser.SplashScreen
         End Sub
     End Class
 End Namespace
