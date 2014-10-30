becf406bd3091ab6b1ec93a12f6615583b9a7707
bionicbone <github@bionicbone.plus.com>
2014-10-30 06:08:39 +0000

    ATT Code
    It turned out that ATT still has 7 data bits but they have been moved
    around and NavYaw has been removed.
    Fixed.
    Still not ready for release until more v3.2 testing has been done.

---
+++ AMP Log Analiser x1/Classes/modATT_Checks_v3_2.vb
@@ -0,0 +1,26 @@
+﻿Module modATT_Checks_v3_2
+    Public Sub ATT_Checks_v3_2()
+
+        'This part only check the correct data structure of the data line
+        'and prepares the variables ready for the MainAnalysis which
+        'is called directly after by this code.
+
+        ' THIS CODE MUST ONLY BE CALLED AFTER THE DATA LINE HAS BEEN VALIDATED AS THE CORRECT TYPE !!!
+
+        If ReadFileResilienceCheck(7) = True Then
+            'v3.1 - FMT, 1, 17, ATT, cccccCC, RollIn,Roll,PitchIn,Pitch,YawIn,Yaw,NavYaw
+            'v3.2 - FMT, 1, 19, ATT, IccccCC, TimeMS,DesRoll,Roll,DesPitch,Pitch,DesYaw,Yaw
+            Log_ATT_RollIn = Val(DataArray(2))
+            Log_ATT_Roll = Val(DataArray(3))
+            Log_ATT_PitchIn = Val(DataArray(4))
+            Log_ATT_Pitch = Val(DataArray(5))
+            Log_ATT_YawIn = Val(DataArray(6))
+            Log_ATT_Yaw = Val(DataArray(7))
+            Log_ATT_NavYaw = 0                          'Value is not available in v3.2 ATT Dataline
+
+        End If
+
+        Call ATT_MainAnalysis()
+
+    End Sub
+End Module
---
+++ AMP Log Analiser x1/Classes/modATT_MainAnalysis.vb
@@ -0,0 +1,34 @@
+﻿Module modATT_MainAnalysis
+    Public Sub ATT_MainAnalysis()
+
+        ' This part only checks the already preset Variables for the data line.
+
+        ' THIS CODE MUST ONLY BE CALLED AFTER THE DATA LINE VARIABLES HAVE BEEN SET !!!
+
+        'Track to ensure Roll is following RollIn and Pitch is following PitchIn.
+        If Log_In_Flight = True Then
+            If Log_ATT_RollIn <> 0 And Log_ATT_RollIn - Log_ATT_Roll > TempMaxRollDiff Then TempMaxRollDiff = Log_ATT_RollIn - Log_ATT_Roll
+            If Log_ATT_PitchIn <> 0 And Log_ATT_PitchIn - Log_ATT_Pitch > TempMaxPitchDiff Then TempMaxPitchDiff = Log_ATT_PitchIn - Log_ATT_Pitch
+            'Debug.Print("Log_In_Flight = " & Log_In_Flight)
+            'Debug.Print("Diff RollIn and Roll = " & Format(Log_ATT_RollIn - Log_ATT_Roll, "000000") & " Max = " & TempMaxRollDiff _
+            '& " : PicthIn and Pitch = " & Format(Log_ATT_PitchIn - Log_ATT_Pitch, "000000") _
+            '& " Max = " & TempMaxPitchDiff)
+        End If
+
+        ' Update the Attitude Chart
+        frmMainForm.chartAttitude.Series("RollIn").Points.AddY(Log_ATT_RollIn)
+        frmMainForm.chartAttitude.Series("Roll").Points.AddY(Log_ATT_Roll)
+        frmMainForm.chartAttitude.Series("NavRoll").Points.AddY(Log_NTUN_DRol)
+        frmMainForm.chartAttitude.Series("PitchIn").Points.AddY(Log_ATT_PitchIn)
+        frmMainForm.chartAttitude.Series("Pitch").Points.AddY(Log_ATT_Pitch)
+        frmMainForm.chartAttitude.Series("NavPitch").Points.AddY(Log_NTUN_DPit)
+        frmMainForm.chartAttitude.Series("Speed").Points.AddY(Log_GPS_Spd)
+        frmMainForm.chartAttitude.Series("YawIn").Points.AddY(Log_ATT_YawIn)
+        frmMainForm.chartAttitude.Series("Yaw").Points.AddY(Log_ATT_Yaw)
+        frmMainForm.chartAttitude.Series("NavYaw").Points.AddY(Log_ATT_NavYaw)
+        frmMainForm.chartAttitude.Series("Travel").Points.AddY(GPS_Calculated_Direction)
+        frmMainForm.chartAttitude.Series("ClimbIn").Points.AddY(Log_CTUN_ThrIn)
+        frmMainForm.chartAttitude.Series("NavClimb").Points.AddY(Log_CTUN_CRate)
+        frmMainForm.chartAttitude.Series("Altitude").Points.AddY(Log_CTUN_BarAlt * 10)
+    End Sub
+End Module
