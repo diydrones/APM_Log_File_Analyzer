d1bbbab1bacdf244cf3d143e995b2fa147c993a1
Kevin Guest <PublicGitHub@bionicbone.plus.com>
2015-04-22 05:24:50 +0100

    Add compatibility for v3.3 Mode Checks
    v3.3 adds the TimeMS as the 2nd parameter in the MODE line

---
+++ AMP Log Analiser x1/Classes/modMODE_Checks_v3_3.vb
@@ -0,0 +1,19 @@
+﻿Module modMODE_Checks_v3_3
+
+    Sub MODE_Checks_v3_3()
+        'This part only check the correct data structure of the data line
+        'and prepares the variables ready for the MainAnalysis which
+        'is called directly after by this code.
+
+        ' THIS CODE MUST ONLY BE CALLED AFTER THE DATA LINE HAS BEEN VALIDATED AS THE CORRECT TYPE !!!
+
+        ' ReadFileResilienceCheck(3) Can not be used here because the value is not numeric
+
+        Log_Current_Mode = DataArray(2)
+
+        Call MODE_MainAnalysis()
+
+    End Sub
+
+
+End Module
---
+++ AMP Log Analiser x1/Classes/modMODE_MainAnalysis.vb
@@ -0,0 +1,38 @@
+﻿Module modMODE_MainAnalysis
+
+    Public Sub MODE_MainAnalysis()
+
+        ' This part only checks the already preset Variables for the data line.
+
+        ' THIS CODE MUST ONLY BE CALLED AFTER THE DATA LINE VARIABLES HAVE BEEN SET !!!
+
+        If frmMainForm.chkboxSplitModeLandings.Checked = True Then Call AddModeTime()
+
+        'Debug.Print("@ Logline " & DataLine & " - Mode Changed to: " & Log_Current_Mode & " @ " & Log_GPS_DateTime)
+
+        WriteTextLog("")
+        WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": Mode Changed to " & Log_Current_Mode)
+        WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": Mode Initialised at " & Log_GPS_Lat & " " & Log_GPS_Lng & " Alt: " & Log_CTUN_BarAlt & " Spd:" & Log_GPS_Spd)
+
+        'Store the Total_Current used so far when the Mode Changes, thus Log_Total_Current - Log_Mode_Start_Current
+        'will = the Total_Mode_Current
+        Log_Mode_Start_Current = Log_Total_Current
+
+        If Log_In_Flight = True Then Mode_In_Flight_Start_Time = Log_GPS_DateTime
+
+        'Store the current time as the mode changes and store the new mode.
+        Log_Last_Mode_Changed_DateTime = Log_GPS_DateTime
+        'Debug.Print("Log_Last_Mode_Changed_DateTime: " & Log_Last_Mode_Changed_DateTime)
+
+        'Initialise the Mode Time and Flight Time Variables
+        Log_Current_Mode_Time = 0
+        Log_Current_Mode_Flight_Time = 0
+        PM_Delay_Counter = 0
+        'Debug.Print("Log_Current_Mode_Time (inc Ground Time) = " & Log_Current_Mode_Time)
+        'Debug.Print("Log_Current_Mode_Flight_Time (Flying Only Reset): " & Log_Current_Mode_Flight_Time)
+        'Debug.Print("----------------------------------------------------------------------------------------")
+        'Debug.Print("")
+
+    End Sub
+
+End Module
