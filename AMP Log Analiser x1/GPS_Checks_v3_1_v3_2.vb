068053322417a6f9508d2741579ac16b11920d96
Kevin Guest <PublicGitHub@bionicbone.plus.com>
2015-12-19 04:44:20 +0000

    v3.3 GPS Checks
    Added support for V3.3 GPS checks.
    Changed current code to complete version checks first, then setup the
    variables before calling the main analysis.

---
+++ AMP Log Analiser x1/GPS_Checks_v3_1_v3_2.vb
@@ -0,0 +1,32 @@
+﻿Module modGPS_Checks_v3_1_v3_2
+
+    Public Sub GPS_Checks_v3_1_v3_2()
+
+        'This part only check the correct data structure of the data line
+        'and prepares the variables ready for the MainAnalysis which
+        'is called directly after by this code.
+
+        ' THIS CODE MUST ONLY BE CALLED AFTER THE DATA LINE HAS BEEN VALIDATED AS THE CORRECT TYPE !!!
+
+        'An IMU value should have only 7 pieces of numeric data!
+
+        'v3.2 - FMT, 130, 45, GPS, BIHBcLLeeEefI, Status,TimeMS,Week,NSats,HDop,Lat,Lng,RelAlt,Alt,Spd,GCrs,VZ,T
+
+
+        If ReadFileResilienceCheck(13) = True Then
+            Log_GPS_Status = Val(DataArray(1))
+            Log_GPS_TimeMS = Val(DataArray(2))
+            Log_GPS_Week = Val(DataArray(3))
+            Log_GPS_NSats = Val(DataArray(4))
+            Log_GPS_HDop = Val(DataArray(5))
+            Log_GPS_Lat = Val(DataArray(6))
+            Log_GPS_Lng = Val(DataArray(7))
+            Log_GPS_Alt = Val(DataArray(9))
+            Log_GPS_Spd = Val(DataArray(10))
+            Log_GPS_GCrs = Val(DataArray(11))
+            Call GPS_MainAnalysis()
+        End If
+
+    End Sub
+
+End Module
