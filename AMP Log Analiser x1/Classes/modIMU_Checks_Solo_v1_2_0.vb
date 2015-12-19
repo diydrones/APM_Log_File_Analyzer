a0e0d87320a98d6c8a40dfb7968f9525022830df
Kevin Guest <PublicGitHub@bionicbone.plus.com>
2015-12-19 17:46:25 +0000

    Solo v1.2.0 support and Bug fixes
    Solo uses the same format as v3.2 except for the IMU data which just has
    a few more fields.
    Some bugs were introduced while adding support for v3.3 that made v3.2
    PARMs fail.

---
+++ AMP Log Analiser x1/Classes/modIMU_Checks_Solo_v1_2_0.vb
@@ -0,0 +1,31 @@
+﻿Module modIMU_Checks_Solo_v1_2_0
+
+    Public Sub IMU_Checks_Solo_v1_2_0()
+
+        'This part only check the correct data structure of the data line
+        'and prepares the variables ready for the MainAnalysis which
+        'is called directly after by this code.
+
+        ' THIS CODE MUST ONLY BE CALLED AFTER THE DATA LINE HAS BEEN VALIDATED AS THE CORRECT TYPE !!!
+
+        'An IMU value should have only 7 pieces of numeric data!
+
+        'APM:Copter v3.2 - FMT, 131, 31, IMU, Iffffff, TimeMS,GyrX,GyrY,GyrZ,AccX,AccY,AccZ
+        'APM:Copter v3.3 - FMT, 132, 49, IMU, QffffffIIfBB, TimeUS,GyrX,GyrY,GyrZ,AccX,AccY,AccZ,ErrG,ErrA,Temp,GyHlt,AcHlt
+        '    Solo v1.2.0 - FMT, 131, 43, IMU, IffffffIIf, TimeMS,GyrX,GyrY,GyrZ,AccX,AccY,AccZ,ErrG,ErrA,Temp
+
+        If ReadFileResilienceCheck(10) = True Then
+            'Debug.Print("IMU Data Detected...")
+            Log_IMU_TimeMS = Val(DataArray(1))
+            Log_IMU_GyrX = Val(DataArray(2))
+            Log_IMU_GyrY = Val(DataArray(3))
+            Log_IMU_GyrZ = Val(DataArray(4))
+            Log_IMU_AccX = Val(DataArray(5))
+            Log_IMU_AccY = Val(DataArray(6))
+            Log_IMU_AccZ = Val(DataArray(7))
+            Call IMU_MainAnalysis()
+        End If
+
+    End Sub
+
+End Module
