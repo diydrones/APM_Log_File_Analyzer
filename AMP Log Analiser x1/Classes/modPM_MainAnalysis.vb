85ee5904c8fdbf55f566aaf4e6fcb793dce4346c
Kevin Guest <PublicGitHub@bionicbone.plus.com>
2015-12-28 10:23:31 +0000

    Issue #22 GCS Heatbeat
    Code better warnings for heatbeat levels depending on if the failsafe is
    set.

--- AMP Log Analiser x1/Classes/modPM_MainAnalysis.vb
+++ AMP Log Analiser x1/Classes/modPM_MainAnalysis.vb
@@ -40,31 +40,56 @@
             End If
         End If
 
+
+        ' GCS monitoring
         If Log_PM_PMT = 10 And Log_GCS_Attached = False Then
             Log_GCS_Attached = True
             WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Information: Ground Command Station is Attached.")
             Debug.Print("GCS Info: Ground Command Station Initialised.")
         End If
 
-        If Log_PM_PMT <> PM_Last_PMT And Log_PM_PMT < 9 And Log_GCS_Attached = True Then
+        ' GCS monitoring when GCS failsafe is Activated
+        If Log_PM_PMT <> PM_Last_PMT And Log_PM_PMT < 9 And Log_GCS_Attached = True And PARM_FS_GCS_ENABLE > 0 Then
             If Log_PM_PMT = 0 Then
                 WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: GCS Heartbeat signal has been lost!")
                 WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: Current Distance from Launch is " & Format(Dist_From_Launch * 1000, "0.00") & "m ~ " & Format(Dist_From_Launch * 3280.8399, "0.00") & "ft")
                 WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: Current Direction of UAV is " & Log_GPS_GCrs & " degrees (0 = north).")
                 Debug.Print("GCS Heartbeat Error: GCS Heartbeat signal has been lost!")
-            Else
+            ElseIf Log_PM_PMT >= 1 And Log_PM_PMT <= 4 Then
                 WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: GCS Heartbeat signal is low @ " & Log_PM_PMT & "0%")
                 WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: Current Distance from Launch is " & Format(Dist_From_Launch * 1000, "0.00") & "m ~ " & Format(Dist_From_Launch * 3280.8399, "0.00") & "ft")
                 WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: Current Direction of UAV is " & Log_GPS_GCrs & " degrees (0 = north).")
+                Debug.Print("GCS Heartbeat Error: GCS Heartbeat in inconsistent @ " & Log_PM_PMT)
+            Else
+                WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Warning: GCS Heartbeat signal is low @ " & Log_PM_PMT & "0%")
+                WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Warning: Current Distance from Launch is " & Format(Dist_From_Launch * 1000, "0.00") & "m ~ " & Format(Dist_From_Launch * 3280.8399, "0.00") & "ft")
+                WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Warning: Current Direction of UAV is " & Log_GPS_GCrs & " degrees (0 = north).")
                 Debug.Print("GCS Heartbeat Error: GCS Heartbeat in inconsistent @ " & Log_PM_PMT)
             End If
             PM_Last_PMT = Log_PM_PMT
         Else
             If PM_Last_PMT < 9 And Log_PM_PMT >= 9 Then
-                WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: GCS Heartbeat signal is low now OK @ 100%")
+                WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: GCS Heartbeat signal is now OK @ 100%")
                 PM_Last_PMT = Log_PM_PMT
             End If
         End If
+
+        ' GCS monitoring when GCS failsafe is NOT Activated
+        If Log_PM_PMT <> PM_Last_PMT And Log_PM_PMT < 9 And Log_GCS_Attached = True And PARM_FS_GCS_ENABLE > 0 Then
+            If Log_PM_PMT = 0 Then
+                WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: GCS Heartbeat signal has been lost!")
+                WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: Current Distance from Launch is " & Format(Dist_From_Launch * 1000, "0.00") & "m ~ " & Format(Dist_From_Launch * 3280.8399, "0.00") & "ft")
+                WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: Current Direction of UAV is " & Log_GPS_GCrs & " degrees (0 = north).")
+                Debug.Print("GCS Heartbeat Error: GCS Heartbeat signal has been lost!")
+            End If
+            PM_Last_PMT = Log_PM_PMT
+        Else
+            If PM_Last_PMT < 9 And Log_PM_PMT >= 9 Then
+                WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": GCS Heartbeat Error: GCS Heartbeat signal is now OK @ 100%")
+                PM_Last_PMT = Log_PM_PMT
+            End If
+        End If
+
 
         If Log_PM_I2CErr <> 0 Then
             WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": I2C Error: At least one I2C error has been detected.")
@@ -79,23 +104,6 @@
             WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": INS Error: was disconnected from power before being disabled / disconnected.")
             Debug.Print("INS Error: Current Value of PM Parameter Log_PM_INSErr is " & Log_PM_INSErr)
         End If
-
-        'KXG 08/06/2014 - INAVerrCODE REMOVED
-        'The following code has been removed as it was found to give many results which can not be explained
-        'due to a lack of information about this value.
-        '
-        'If Log_PM_INAVErr <> 0 And Log_PM_INAVErr <> PM_Last_INAVErr Then
-        '    PM_Last_INAVErr = Log_PM_INAVErr
-        '    WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": INAV Error: A navigation error has been detected.")
-        '    WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": INAV Error: Current Value of PM Parameter PM_INAVErr is " & Log_PM_INAVErr)
-        '    WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": INAV Error: An update will be displayed if the value changes.")
-        '    Debug.Print("INAV Error: Current Value of PM Parameter Log_PM_INAVErr is " & Log_PM_INAVErr)
-        'Else
-        '    If Log_PM_INAVErr = 0 And PM_Last_INAVErr <> 0 Then
-        '        PM_Last_INAVErr = Log_PM_INAVErr
-        '        WriteTextLog(Log_GPS_DateTime & " - " & Format(DataLine, "000000") & ": INAV Error: Cleared")
-        '    End If
-        'End If
 
     End Sub
 End Module
